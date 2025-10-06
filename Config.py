32d16a7146f4d44693d9b5f9bd1d15b25cd08ec3
Tyrantanos <fl6913409@gmail.com>
2025-10-06 11:20:22 +0530

    Config.py

--- Config.py
+++ Config.py
@@ -3,16 +3,16 @@
 import os
 
 # Bot token from @botfather
-BOT_TOKEN = ""
+BOT_TOKEN = "8432811579:AAF7z9MIW8DapEckt_6xZ-bpFZeaYCzg9f8"
 # From my.telegram.org/
-API_ID = ""
-API_HASH =  ""
+API_ID = "26918615"
+API_HASH =  "6f2096f023dad9d58ecfb9fdafa48ca1"
 # For /log cmd
 OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "969084369").split(" ")]
 # No time limit for this users
-AUTH_USERS = [969084369, 2800583882]
+AUTH_USERS = [7514677608]
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
-BOT_USERNAME='test' #bot username without@
+BOT_USERNAME='DEMONANOSBOT' #bot username without@
 API_TOKEN='b7d9e1f7-12a6-4b8e-89a2-c3d124e5f6a7' #don't change this without developer permission
 
